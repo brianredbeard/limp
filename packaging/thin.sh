d368c2bcd59679b081d9578b0f3f42c6c08aba6b
brian redbeard <bharrington@alticon.net>
2011-06-20 19:21:15 -0400

    made change to remove huge logo and replace with smaller amount of text

--- packaging/thin.sh
+++ packaging/thin.sh
@@ -1,1 +1,5 @@
+#Remove donation header
 sed -i 's/\$content\[.SHOW_DONATEBUTTON.\] = true;/\$content\[.SHOW_DONATEBUTTON.\] = false;/g' ../src/include/functions_common.php
+
+#Remove logo and replace with simple text
+sed -i 's/<img src="{EXTRA_PHPLOGCON_LOGO}" width="500" height="79" name="HeaderLogo">/LogAnalyzer/g' ../src/templates/include_header.html
